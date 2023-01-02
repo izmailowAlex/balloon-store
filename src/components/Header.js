ed9a0b33ffd0764ea0b0f488404ed30c337d7dfd
gsidnick <gsidnick@gmail.com>
2023-01-02 03:23:44 +0300

    refactor: update logo image to svg

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,13 @@
 import { Link } from 'react-router-dom';
 import Nav from './Header/Nav';
 import Order from './Header/Order';
+import Logo from '../images/logo.svg';
 
 function Header() {
     return (
       <div className="header container">
         <Nav />
-        <Link className="header__logo" to='/'><img src="../Logo.png" /></Link>
+        <Link className="header__logo" to='/'><img src={Logo} alt="Balloon Logo"/></Link>
         <Order />
       </div>
     );
