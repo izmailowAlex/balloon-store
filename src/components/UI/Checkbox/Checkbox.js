b0886198ae3820debcfa8d031766e5f2921eb5b7
gsidnick <gsidnick@gmail.com>
2023-01-02 20:38:01 +0300

    feat: add props className to checkbox component

--- src/components/UI/Checkbox/Checkbox.js
+++ src/components/UI/Checkbox/Checkbox.js
@@ -1,14 +1,16 @@
 import './Checkbox.css';
 
-function Checkbox(props) {
+function Checkbox({children, className}) {
+  const classname = className ? "checkbox " + className : "checkbox"
+
   return (
-    <label className="checkbox">
+    <label className={classname}>
       <input className="checkbox__input" type="checkbox" />
       <span className="checkbox__wrapper">
         <span className="checkbox__icon">
           <svg className="checkbox__svg"><use href="#check"></use></svg>
         </span>
-        <span className="checkbox__label">{props.children}</span>
+        <span className="checkbox__label">{children}</span>
       </span>
     </label>
   );
