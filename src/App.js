765374c050f5f2fa4543ef9d4d2e1afcb1e67e4b
gsidnick <gsidnick@gmail.com>
2023-01-03 01:37:19 +0300

    feat: add decorative elements

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import Header from './components/Header';
 import Main from './components/Main';
 import Footer from './components/Footer';
+import './App.css';
 
 function App() {
   return (
@@ -12,6 +13,9 @@ function App() {
         <Main />
         <Footer />
       </Router>
+      <span className="balloon__circle balloon_red"></span>
+      <span className="balloon__circle balloon_yellow"></span>
+      <span className="balloon__circle balloon_blue"></span>
     </div>
   );
 }
