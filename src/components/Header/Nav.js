72db533bd490b8a560748f0ea0fc7477acff9670
gsidnick <gsidnick@gmail.com>
2023-01-04 01:57:16 +0300

    refactor: change nav styles

--- src/components/Header/Nav.js
+++ src/components/Header/Nav.js
@@ -1,16 +1,25 @@
 import { Link } from 'react-router-dom';
+import './Nav.css';
 
-function Header() {
-    return (
-        <nav className="header__nav">
-            <ul className="header__nav-list">
-                <li><Link to="catalog">Каталог</Link></li>
-                <li><Link to="home">Доставка</Link></li>
-                <li><Link to="#">Контакты</Link></li>
-                <li><Link to="#">Галерея</Link></li>
-            </ul>
-        </nav>
-    );
+function Nav() {
+  return (
+    <nav className="nav">
+      <ul className="nav__list">
+        <li className="nav__item">
+          <Link className="nav__link" to="catalog">Каталог</Link>
+        </li>
+        <li className="nav__item">
+          <Link className="nav__link" to="home">Доставка</Link>
+        </li>
+        <li className="nav__item">
+          <Link className="nav__link" to="#">Контакты</Link>
+        </li>
+        <li className="nav__item">
+          <Link className="nav__link" to="#">Галерея</Link>
+        </li>
+      </ul>
+    </nav>
+  );
 }
   
-export default Header;
+export default Nav;
