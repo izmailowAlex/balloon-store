578515d8c3c0b5335afa3ba4208d023d64667221
gsidnick <gsidnick@gmail.com>
2023-01-04 20:55:22 +0300

    feat: set maxlength attribute to inputs

--- src/components/Main/Checkout.js
+++ src/components/Main/Checkout.js
@@ -8,15 +8,15 @@ function Checkout() {
       <div className="checkout__form">
         <div className="checkout__wrapper">
           <h2 className="checkout__title">Оформление заказа</h2>
-          <Input className="checkout__input" name="name" placeholder="Имя"/>
-          <Input className="checkout__input" name="phone" placeholder="Телефон"/>
-          <Input className="checkout__input" name="address" placeholder="Адрес доставки"/>
+          <Input className="checkout__input" name="name" placeholder="Имя" maxlength={30}/>
+          <Input className="checkout__input" name="phone" placeholder="Телефон" maxlength={11}/>
+          <Input className="checkout__input" name="address" placeholder="Адрес доставки" maxlength={50}/>
           <div className="checkout__credit-card credit-card">
-            <Input className="credit-card__input" name="cardHolderName" placeholder="CARDHOLDER NAME"/>
-            <Input className="credit-card__input" name="cardNumber" placeholder="1234 5678 9012 3456"/>
+            <Input className="credit-card__input" name="cardHolderName" placeholder="CARDHOLDER NAME" maxlength={30}/>
+            <Input className="credit-card__input" name="cardNumber" placeholder="1234 5678 9012 3456" maxlength={16}/>
             <div className="credit-card__expiration">
-              <Input className="credit-card__input" name="cardDate" placeholder="MM / YY"/>
-              <Input className="credit-card__input" name="cardCVC" placeholder="CVC"/>
+              <Input className="credit-card__input" name="cardDate" placeholder="MM / YY" maxlength={4}/>
+              <Input className="credit-card__input" name="cardCVC" placeholder="CVC" maxlength={3}/>
             </div>
           </div>
         </div>
