6ab265a61315466a8ee27f1b64a0ced1b4bd0ded
gsidnick <gsidnick@gmail.com>
2023-01-05 01:54:06 +0300

    feat: add counter functionality

--- src/components/UI/Counter/Counter.js
+++ src/components/UI/Counter/Counter.js
@@ -1,15 +1,31 @@
+import { useRef } from 'react';
+
 import './Counter.css';
 
-function Counter({quantity}) {
+function Counter({count}) {
+
+  const inputRef = useRef();
+
+  function changeCountValue(operation) {
+    if (operation === 'plus') count++;
+    else count--;
+    const inputElem = inputRef.current;
+    inputElem.value = count;
+  }
+
   return (
     <div className="counter">
-      <button className="counter__button-minus">
+      <button onClick={() => {
+        changeCountValue('minus');
+      }} className="counter__button-minus">
         <svg className="counter__svg">
           <use href="#minus"></use>
         </svg>
       </button>
-      <input className="counter__value" type="text" value={quantity}/>
-      <button className="counter__button-plus">
+      <input ref={inputRef} className="counter__value" type="text" value={count}/>
+      <button onClick={() => {
+        changeCountValue('plus');
+      }} className="counter__button-plus">
         <svg className="counter__svg">
           <use href="#plus"></use>
         </svg>
