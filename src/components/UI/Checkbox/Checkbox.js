4ff7b1187ef8ca0ec28c49e13bff1eb5995ff4e7
gsidnick <gsidnick@gmail.com>
2023-01-07 12:48:55 +0300

    feat: add onChange event to checkbox component

--- src/components/UI/Checkbox/Checkbox.js
+++ src/components/UI/Checkbox/Checkbox.js
@@ -1,11 +1,15 @@
 import './Checkbox.css';
 
-function Checkbox({children, className}) {
+function Checkbox({children, className, onChange}) {
   const classname = className ? "checkbox " + className : "checkbox"
 
   return (
     <label className={classname}>
-      <input className="checkbox__input" type="checkbox" />
+      <input
+        className="checkbox__input"
+        type="checkbox"
+        onChange={onChange}
+      />
       <span className="checkbox__wrapper">
         <span className="checkbox__icon">
           <svg className="checkbox__svg"><use href="#check"></use></svg>
