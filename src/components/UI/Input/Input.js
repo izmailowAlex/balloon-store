18589372aa4acf056551f33510d2ab1e3f5d2815
gsidnick <gsidnick@gmail.com>
2023-01-07 21:28:39 +0300

    feat: add error & success message to input component

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import './Input.css';
 
-function Input({className, name, value, placeholder, label, message, maxlength, onChange, onFocus, onBlur, onKeyDown}, ref) {
+function Input({className, name, value, placeholder, label, error, errorMessage, success, successMessage, maxlength, onChange, onFocus, onBlur, onKeyDown}, ref) {
   let classname = className ? 'input ' + className : 'input';
   classname = label ? classname + ' input_labeled' : classname;
+  classname = error ? classname + ' input_error' : classname;
+  classname = success ? classname + ' input_success' : classname;
+
 
   return (
     <div className={classname}>
@@ -22,7 +25,8 @@ function Input({className, name, value, placeholder, label, message, maxlength, 
         onKeyDown={onKeyDown}
       />
       {label ? <label className="input__label">{label}</label> : ""}
-      {message ? <span className="input__message">{message}</span> : ""}
+      {error ? <span className="input__message">{errorMessage}</span> : ""}
+      {success ? <span className="input__message">{successMessage}</span> : ""}
     </div>
   );
 }
