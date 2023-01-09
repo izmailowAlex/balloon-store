6e13e9ab5639f79d3e3438c20e4752ffc5e31428
gsidnick <gsidnick@gmail.com>
2023-01-10 00:33:53 +0300

    feat: migrate Footer to tsx

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,6 +1,7 @@
+import React from 'react'
 import './Footer.css'
 
-function Footer () {
+function Footer (): JSX.Element {
   return (
     <footer className="footer">
       <div className="footer__container container">
