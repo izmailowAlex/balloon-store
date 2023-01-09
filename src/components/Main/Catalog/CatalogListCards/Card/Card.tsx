299fc5b62cdd6a6c0e91b2b7825c89444820e3a8
gsidnick <gsidnick@gmail.com>
2023-01-09 21:05:38 +0300

    refactor: migration Card to tsx

--- src/components/Main/Catalog/CatalogListCards/Card/Card.tsx
+++ src/components/Main/Catalog/CatalogListCards/Card/Card.tsx
@@ -1,17 +1,21 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import Counter from '../../../../UI/Counter/Counter'
+import { ICardProps } from '../../../../../interfaces/interface'
 import './Card.css'
 
-function Card ({ /* eslint-disable react/prop-types */product }) {
+function Card ({ product }: ICardProps): JSX.Element {
   return (
     <div className="card">
       <div className="card__image-wrapper">
-        <img className="card__image" src={`../images/${product.image}`} alt=""/>
+        <img
+          className="card__image"
+          src={`../images/${product.image}`}
+          alt=""
+        />
       </div>
       <div className="card__controls">
         <h3 className="card__price">{product.price} ₽</h3>
-        <Counter count={product.count} min={product.min} max={product.max}/>
+        <Counter count={product.count} min={product.min} max={product.max} />
         <button className="card__button"></button>
       </div>
     </div>
--- src/interfaces/interface.ts
+++ src/interfaces/interface.ts
@@ -10,4 +10,27 @@ export interface ICounterProps {
   max: number
 }
 
+export interface ICardProps {
+  product: IProduct
+}
+
+export interface IProduct {
+  id: string
+  title: string
+  image: string
+  count: number
+  min: number
+  max: number
+  price: number
+  instock: boolean
+  discount: number
+  category: string
+  type: string
+  color: string
+  size: string
+  producer: string
+  country: string
+  description: string
+}
+
 export type Callback<T> = (data?: T) => void
