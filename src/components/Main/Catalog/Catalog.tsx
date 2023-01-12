71c9da6b9518051ad82560f5a7672a11829e3580
gsidnick <gsidnick@gmail.com>
2023-01-12 10:28:43 +0300

    feat: add ICatalogContext interface

--- src/components/Main/Catalog/Catalog.tsx
+++ src/components/Main/Catalog/Catalog.tsx
@@ -3,17 +3,22 @@ import { AppContext } from '../../../App'
 import Filter from './CatalogListCards/Filter/Filter'
 import CatalogListCards from './CatalogListCards/CatalogListCards'
 import './Catalog.css'
+import { IProduct } from '../../../interfaces/interface'
 
-export const CatalogContext = React.createContext({})
+interface ICatalogContext {
+  filteredList: IProduct[]
+}
 
-function Catalog () {
+export const CatalogContext = React.createContext({} as ICatalogContext)
+
+function Catalog (): JSX.Element {
   const { productsLibrary } = useContext(AppContext)
-  const [filteredList, setFilteredList] = useState(productsLibrary)
+  const [filteredList, setFilteredList] = useState<IProduct[]>(productsLibrary)
   return (
     <div className="main__catalog">
       <h2 className="main__catalog-title">Каталог шаров</h2>
       <div className="main__catalog-contain">
-        <CatalogContext.Provider value={{ filteredList, setFilteredList }}>
+        <CatalogContext.Provider value={{ filteredList }}>
           <Filter />
           <CatalogListCards />
         </CatalogContext.Provider>
