1a5c07e3a7d7bf6627c59ceb377675f37cdaceb8
Aleks_bass <105873742+izmailowAlex@users.noreply.github.com>
2023-02-11 18:54:19 +0300

    fix: fix duplicate type

--- src/interfaces/interface.tsx
+++ src/interfaces/interface.tsx
@@ -59,25 +59,6 @@ export interface IProduct {
   description: string
 }
 
-export interface IProduct {
-  id: string
-  title: string
-  image: string
-  count: number
-  min: number
-  max: number
-  price: number
-  instock: boolean
-  discount: number
-  category: string
-  type: string
-  color: string
-  size: string
-  producer: string
-  country: string
-  description: string
-}
-
 export interface IProductsLibrary {
   productsLibrary: IProduct[]
 }
